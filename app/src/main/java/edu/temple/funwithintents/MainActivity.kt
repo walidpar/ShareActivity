03f0c269a9876204b21d15b928be52c6984cd7aa
Walid Parast <walidparast1@gmail.com>
2025-04-01 11:14:27 -0400

    Checked if editText is null, began Intent.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -15,7 +16,11 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            if (editText != null) {
+                Intent(Intent.ACTION_VIEW).apply {
+                    //Intent.ACTION_CHOOSER
+                }
+            }
         }
     }
 }
